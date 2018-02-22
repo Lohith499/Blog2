c4f37a0d2124025ae464eed9b3d79d058a45257f
Lohith499 <lohith.gre@gmail.com>
2018-02-22 09:12:00 -0500

    enums

--- db/migrate/20180222130331_create_bugs.rb
+++ db/migrate/20180222130331_create_bugs.rb
@@ -3,9 +3,9 @@ class CreateBugs < ActiveRecord::Migration[5.1]
     create_table :bugs do |t|
       t.string :title
       t.text :articles
-      t.text :issue_type, default: 2
-      t.text :priority, default: 1
-      t.text :status, default: 0
+      t.integer :issue_type, default: 2
+      t.integer :priority, default: 1
+      t.integer :status, default: 0
 
       t.timestamps
     end
