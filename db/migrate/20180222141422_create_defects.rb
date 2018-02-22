9ca48a70ac0f03f8e329001cd8365b5eaaf2b9a7
Lohith499 <lohith.gre@gmail.com>
2018-02-22 17:23:18 -0500

    users add

--- db/migrate/20180222141422_create_defects.rb
+++ db/migrate/20180222141422_create_defects.rb
@@ -3,9 +3,9 @@ class CreateDefects < ActiveRecord::Migration[5.1]
     create_table :defects do |t|
       t.string :title
       t.text :articles
-      t.integer :issue_type
-      t.integer :priority
-      t.integer :status
+      t.integer :issue_type,default: 2
+      t.integer :priority,default: 1
+      t.integer :status,default: 0
 
       t.timestamps
     end
