0c5a901edb519107fdac6fe943ad6183bd0b86e0
Lohith499 <lohith.gre@gmail.com>
2018-02-22 17:27:21 -0500

    users add

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   validates :lname, presence:true
   validates :fname, presence:true
   validates :email, presence:true,uniqueness: {case_sensitive: false}
-  validate :thumbnail, allow_blank:true,format: {with: %r{\.(gif|jpg|png)\Z}i,message: 'must be url for gif or jpg or png'}
+  validates :thumbnail, allow_blank:true,format: {with: %r{\.(gif|jpg|png)\Z}i,message: 'must be url for gif or jpg or png'}
 end
