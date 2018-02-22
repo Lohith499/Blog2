526d7a8d831e1ffdfb9cb41ebb85f0b1e20048e7
Lohith499 <lohith.gre@gmail.com>
2018-02-22 16:56:14 -0500

    validations

--- app/models/defect.rb
+++ app/models/defect.rb
@@ -2,4 +2,8 @@ class Defect < ApplicationRecord
   enum issue_type: [:issue, :enhancement, :feature]
   enum priority: [:low, :medium, :high]
   enum status: [:open, :closed, :monitor]
+
+  validates :title, presence:true
+  validates :articles, presence:true
+  
 end
--- test/models/defect_test.rb
+++ test/models/defect_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
 class DefectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup do
+    @defect=Defect.create(title:"lohith", articles:"first article", issue_type:2, priority:1, status:1)
+  end
+  test "Defect record is valid" do
+    assert @defect.valid?
+  end
+
+  test "title must be present" do
+    @post.title=""
+    assert_not @post.valid?
+    assert
+  end
+
+  test "article must be present" do
+    @post.articles=""
+    assert_not @post.valid?
+    assert
+  end
+
+  test "issue_type should be one of the dropdowm value" do
+    @post.issue_type="unknown"
+    assert_not @post.valid?
+    assert
+  end
+
+  test "priority should be one of the dropdowm value" do
+    @post.issue_prioritytype="unknown"
+    assert_not @post.valid?
+    assert
+  end
+
+
+  test "status should be one of the dropdowm value" do
+    @post.status="unknown"
+    assert_not @post.valid?
+    assert
+  end
+
 end
